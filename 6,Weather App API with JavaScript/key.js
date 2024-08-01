dda086dcf0da37fc0d29c3712f8dd34a4332c3c8
Konosuke0307 <157306793+Konosuke0307@users.noreply.github.com>
2024-08-01 10:36:18 -0400

    Add files via upload

---
+++ 6,Weather App API with JavaScript/key.js
@@ -0,0 +1,2 @@
+//Assign the copied API key to the 'key' variable
+key = "3f57240bd7318a4f50b0f7236ae354d7";
---
+++ 7,Marvel App API in JavaScript/api-data.js
@@ -0,0 +1,3 @@
+let ts = "1681802982683";
+let publicKey = "e700f6a60dcf3571832457c58db10f53";
+let hashVal = "53cf114908ac441edff3bc6cc75643d9";
